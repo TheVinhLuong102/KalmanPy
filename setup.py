0b752bd20710e8988cecdaed812c9299afaf5476
Vinh Luong <vinh@uptake.io>
2015-07-07 15:14:30 -0500

    add PyPI setup files

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 
 setup(name='Kalman',
-      version='0.3',
+      version='0.1.3',
       packages=['Kalman'],
       url='http://github.com/MBALearnsToCode/KalmanPy',
       author='Vinh Luong (a.k.a. MBALearnsToCode)',
       author_email='MBALearnsToCode@UChicago.edu',
       description='Kalman Filters',
-      long_description=open('README.txt').read(),
-      license='MIT License')
+      long_description='(please read README.md on GitHub repo)',
+      license='MIT License',
+      install_requires=['NumPy'],
+      classifiers=[],   # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='kalman filter')
