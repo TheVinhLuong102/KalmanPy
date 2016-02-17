5b7b2a79bce73b1c009e68dc27be0b52a3475465
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-02-17 08:56:35 -0600

    update script for PyPI packaging

--- zzzPyPI.sh
+++ zzzPyPI.sh
@@ -1,7 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+
+# install package locally
 pip install -e .
 # or:
 # python setup.py develop
+
+
+# install Twine & Wheel packages
+pip install --upgrade Twine Wheel
+
+
+# register package on PyPI
 python setup.py sdist bdist_wheel --universal
 python setup.py register
 twine upload dist/*
